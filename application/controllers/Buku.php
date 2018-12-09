2646d132b1deb794b7a8012e1e3124bc974ac8ce
Andi <andisupriadijr@gmail.com>
2018-12-09 08:48:04 +0700

    Initial commit

---
+++ application/controllers/Buku.php
@@ -0,0 +1,140 @@
+<?php
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+/**
+ * 
+ */
+class Buku extends CI_controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Buku_Model');
+		$this->load->model('Kategori_Model');
+		# code...
+	}
+
+	public function index()
+	{
+		$data = 
+		[
+			'title' => 'Data Buku',
+			'sub_title' => 'Buku',
+			'content' => 'buku/index',
+			'show'=>$this->Buku_Model->index()->result()
+
+			];
+			$this->load->view('template/my_template', $data);
+	}
+
+	public function add()
+	{
+		$data = 
+		[
+			'title' => 'Data Buku',
+			'sub_title' => 'Tambah Buku',
+			'content' => 'buku/add',
+			'show_kategori' => $this->Kategori_Model->index()->result()
+			
+		];
+			$this->load->view('template/my_template', $data);
+	}
+
+	public function create()
+	{
+		$judul_buku = $this->input->post('judul_buku');
+		$id_kategori = $this->input->post('id_kategori');
+		$pengarang = $this->input->post('pengarang');
+		$thn_terbit = $this->input->post('thn_terbit');
+		$penerbit = $this->input->post('penerbit');
+		$isbn = $this->input->post('isbn');
+		$Jumlah_buku = $this->input->post('Jumlah_buku');
+		$lokasi = $this->input->post('lokasi');
+
+		$data = array(
+			'judul_buku' => $judul_buku,
+			'id_kategori' => $id_kategori,
+			'pengarang' => $pengarang,
+			'thn_terbit' => $thn_terbit,
+			'penerbit' => $penerbit,
+			'isbn' => $isbn,
+			'Jumlah_buku' => $Jumlah_buku,
+			'lokasi' => $lokasi
+		);
+
+		$create = $this->Buku_Model->create($data);
+
+		if ($create) {
+			$this->session->set_flashdata('pesan_create', true);
+			redirect('buku');
+		}else{
+			$this->session->set_flashdata('pesan_create', false);
+			redirect('buku');
+		}
+	}
+	public function edit($id)
+	{
+		$data = 
+		[
+			'title' => 'Buku',
+			'sub_title' => 'Edit Buku',
+			'content' => 'buku/edit',
+			'show' => $this->Buku_Model->edit($id)->row(),
+			'show_kategori' => $this->Kategori_Model->index()->result()
+		];
+		$this->load->view('template/my_template', $data);
+	}
+	public function update ()
+	{
+		$id_buku = $this->input->post('id_buku');
+
+		$judul_buku = $this->input->post('judul_buku');
+		$id_kategori = $this->input->post('id_kategori');
+		$pengarang = $this->input->post('pengarang');
+		$thn_terbit = $this->input->post('thn_terbit');
+		$penerbit = $this->input->post('penerbit');
+		$isbn = $this->input->post('isbn');
+		$Jumlah_buku = $this->input->post('Jumlah_buku');
+		$lokasi = $this->input->post('lokasi');
+
+		$data = array(
+			'judul_buku' => $judul_buku,
+			'id_kategori' => $id_kategori,
+			'pengarang' => $pengarang,
+			'thn_terbit' => $thn_terbit,
+			'penerbit' => $penerbit,
+			'isbn' => $isbn,
+			'Jumlah_buku' => $Jumlah_buku,
+			'lokasi' => $lokasi
+		);
+
+		$update = $this->Buku_Model->update($data, $id_buku);
+
+		if ($update) {
+			$this->session->set_flashdata('pesan_create', true);
+			redirect('buku');
+		}else{
+			$this->session->set_flashdata('pesan_create', false);
+			redirect('buku');
+		}
+	}
+
+	public function delete($id)
+	{
+		$data = array(
+			'deleted_at' => date('Y-m-d H:i:s', time())
+		);
+
+		$delete = $this->Buku_Model->delete($data, $id);
+
+		if ($delete) {
+			$this->session->set_flashdata('pesan_hapus', true);
+			redirect('buku');
+		}
+		else{
+			$this->session->set_flashdata('pesan_hapus', false);
+			redirect('buku');
+		}
+	}
+}
+
---
+++ application/views/dashboard/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Halaman Home</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+    <br></center><font size="5" color="Navy" face="Cooper Black">Test Halaman Home</font>
+</body>
+</html>
